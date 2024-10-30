a51993db442412a9a83d4131bf5f511252ddb554
Vaibhav Kulkarni <120665380+ivaibhavkulkarni@users.noreply.github.com>
2024-10-30 20:41:04 +0530

    Time: 0 ms (100%), Space: 11.7 MB (14.54%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
+        if root is None:
+            return None
+        
+        root.left, root.right = root.right, root.left
+        
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
+
+def create_tree(values):
+    if not values:
+        return None
+    nodes = [TreeNode(val) if val is not None else None for val in values]
+    for i in range(len(nodes)):
+        if nodes[i] is not None:
+            if 2 * i + 1 < len(nodes):
+                nodes[i].left = nodes[2 * i + 1]
+            if 2 * i + 2 < len(nodes):
+                nodes[i].right = nodes[2 * i + 2]
+    return nodes[0]
+
+def level_order_traversal(root):
+    if not root:
+        return []
+    result, queue = [], [root]
+    while queue:
+        node = queue.pop(0)
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    return result
