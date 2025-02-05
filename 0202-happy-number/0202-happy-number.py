8363bbd78d84624e3b27ce57e0f5243a73824f21
Vaibhav Kulkarni <120665380+ivaibhavkulkarni@users.noreply.github.com>
2025-02-05 21:12:55 +0530

    Time: 0 ms (100%), Space: 12.4 MB (68.16%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -4,10 +4,19 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        seen = set()
-        
-        while n != 1 and n not in seen:
-            seen.add(n)
-            n = sum(int(digit) ** 2 for digit in str(n))
-        
-        return n == 1
+        def get_next(num):
+            total_sum = 0
+            while num > 0:
+                digit = num % 10
+                total_sum += digit ** 2
+                num //= 10
+            return total_sum
+
+        slow = n
+        fast = get_next(n)
+
+        while fast != 1 and slow != fast:
+            slow = get_next(slow)
+            fast = get_next(get_next(fast))
+
+        return fast == 1
